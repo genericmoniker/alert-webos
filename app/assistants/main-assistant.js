6aebab0fc6588636e0d8f3c331323b71324e7b1d
Eric Smith <genericmoniker@users.noreply.github.com>
2010-12-23 03:54:05 +0000

    First commit

--- app/assistants/main-assistant.js
+++ app/assistants/main-assistant.js
@@ -2,8 +2,48 @@ function MainAssistant(argFromPusher) {}
 
 MainAssistant.prototype.setup = function() {
 
+	this.controller.setupWidget("site-selector",
+		this.siteSelectorAttributes = {
+			label: "Site"
+		},
+		this.siteSelectorModel = {
+			choices: []
+	});
+	this.siteSelector = this.controller.get("site-selector");
+	this.siteSelectorHandler = this.handleSiteSelectorChange.bind(this);
+	Mojo.Event.listen(this.siteSelector, Mojo.Event.propertyChange, this.siteSelectorHandler);
+
+	this.controller.setupWidget("camera-list",
+		this.cameraListAttributes = {
+		},
+		this.cameraListModel = {
+			items: []
+	});
+	this.cameraList = this.controller.get("camera-list");
+	this.cameraListHandler = this.handleCameraListTap.bind(this);
+	Mojo.Event.listen(this.cameraList, Mojo.Event.tap, this.cameraListHandler);
+
+	this.loadSitesAndCameras();
 };
 
 MainAssistant.prototype.cleanup = function() {
 
+};
+
+MainAssistant.prototype.loadSitesAndCameras = function() {
+	serviceLocator.siteService.loadSites(
+		// Success
+		function() {
+			this.siteSelectorModel.choices = serviceLocator.siteService.sites;
+			this.controller.modelChanged(this.siteSelectorModel);
+		}.bind(this),
+		// Failure
+		function(response) {
+		});
+};
+
+MainAssistant.prototype.handleSiteSelectorChange = function(event) {
+};
+
+MainAssistant.prototype.handleCameraListTap = function(event) {
 };
