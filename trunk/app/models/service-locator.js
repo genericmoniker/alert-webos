38501a3bef004b294a92770636d4022ababfbc33
Eric <Eric@a7eed243-21a3-4537-8924-d26e929ae1cf>
2010-12-29 04:51:49 +0000

    Live video hooked up (but hard to tell if working -- requests for video are timing out).

--- trunk/app/models/service-locator.js
+++ trunk/app/models/service-locator.js
@@ -6,10 +6,12 @@ function ServiceLocator() {
 	this.authService = new AuthService();
 	this.localStorage = new LocalStorage();
 	this.siteService = new SiteService();
+	this.videoService = new VideoService();
 
 	// Resolve dependencies
 	this.authService.httpClient = this.httpClient;
 	this.authService.localStorage = this.localStorage;
 	this.siteService.httpClient = this.httpClient;
+	this.videoService.httpClient = this.httpClient;
 }
 
