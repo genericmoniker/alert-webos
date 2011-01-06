e529a962c75f63de353ded9f771bd497248bf8a9
Eric <Eric@a7eed243-21a3-4537-8924-d26e929ae1cf>
2011-01-06 05:12:51 +0000

    More robust auth service (knows user name) and account preferences (shows username).

--- trunk/app/assistants/prefs-assistant.js
+++ trunk/app/assistants/prefs-assistant.js
@@ -1,16 +1,25 @@
 function PrefsAssistant(argFromPusher) {}
 
 PrefsAssistant.prototype.setup = function() {
+
+	var auth = serviceLocator.authService;
+
 	this.controller.setupWidget("logout-button",
 	this.logoutButtonAttributes = {
 	},
 	this.logoutButtonModel = {
-		label: "Logout"
+		label: "Logout",
+		disabled: !auth.userIsAuthenticated
 	});
 	this.logoutButton = this.controller.get("logout-button");
 	this.logoutHandler = this.handleLogout.bind(this);
 	Mojo.Event.listen(this.logoutButton, Mojo.Event.tap, this.logoutHandler);
 
+	if (auth.userIsAuthenticated) {
+		$("username").innerHTML = auth.username;
+	} else {
+		$("username").innerHTML = "Not logged in";
+	}
 };
 
 PrefsAssistant.prototype.cleanup = function() {
--- trunk/app/assistants/stage-assistant.js
+++ trunk/app/assistants/stage-assistant.js
@@ -10,4 +10,29 @@ StageAssistant.prototype.setup = function() {
 	
 	this.controller.swapScene({name: "welcome", disableSceneScroller: true});
 	this.controller.setWindowOrientation("free");
+	
+	// Application menu model
+	this.appMenuModel = {
+		visible: true
+	};
+};
+
+StageAssistant.prototype.handleCommand = function(event) {
+	if (event.type == Mojo.Event.commandEnable) {
+		if (event.command == Mojo.Menu.helpCmd) {
+			event.stopPropagation(); // Enable the Help menu.
+		} else if (event.command == Mojo.Menu.prefsCmd) {
+			event.stopPropagation(); // Enable the Preferences menu.
+		}
+	}
+	else if (event.type === Mojo.Event.command) {
+		switch (event.command) {
+		case Mojo.Menu.prefsCmd:
+			this.controller.pushScene("prefs");
+			break;
+		case Mojo.Menu.helpCmd:
+			this.controller.pushAppSupportInfoScene();
+			break;
+		}
+	}
 };
