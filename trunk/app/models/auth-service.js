5d28fcb42e8081c13bad170972460dac605ce1ff
Eric <Eric@a7eed243-21a3-4537-8924-d26e929ae1cf>
2011-06-24 00:23:13 +0000

    Fixed the prefs scene.

--- trunk/app/models/auth-service.js
+++ trunk/app/models/auth-service.js
@@ -77,9 +77,13 @@ var authServiceCtor = function(spec) {
 		userIsAuthenticated = false;
 		username = "";
 	};
-	
+
 	var getUserIsAuthenticated = function() {
 		return userIsAuthenticated;
+	};
+
+	var getUsername = function() {
+		return username;
 	};
 
 	// Public Interface
@@ -87,85 +91,7 @@ var authServiceCtor = function(spec) {
 		authenticateFromStorage: authenticateFromStorage,
 		authenticate: authenticate,
 		logout: logout,
-		getUserIsAuthenticated: getUserIsAuthenticated
+		getUserIsAuthenticated: getUserIsAuthenticated,
+		getUsername: getUsername
 	};
 };
-
-/*
-function AuthService() {
-	this.httpClient = null;
-	this.localStorage = null;
-	this.userIsAuthenticated = false;
-	this.username = "";
-}
-
-AuthService.prototype.authenticateFromStorage = function(onSuccess, onFailure, onCredentialsNeeded) {
-	var authToken = this.localStorage.getValue("authToken");
-	
-	// Note: We don't check for null to handle undefined case too.
-	if (authToken) {
-		this.username = this.localStorage.getValue("username");
-		this.httpClient.authToken = authToken;
-		this.httpClient.get("membership.svc/validate", null, false, 
-			// Success
-			function(transport) {
-				this.userIsAuthenticated = true;
-				onSuccess(transport);
-			}.bind(this),
-			
-			// Failure
-			onFailure);
-	}	else {
-		if (onCredentialsNeeded) {
-			onCredentialsNeeded();
-		}
-	}
-};
-
-AuthService.prototype.authenticate = function(username, password, persist, onSuccess, onFailure) {
-
-	var authInfo = "<AuthInfo><UserName>" +
-		this.xmlEscape(username) + 
-		"</UserName><Password>" +
-		this.xmlEscape(password) +
-		"</Password></AuthInfo>"; 
-		
-	this.httpClient.post("membership.svc/authenticate", null, authInfo, true,
-
-		// Success
-		function(transport) {
-			var authToken = transport.getHeader("X-Authorization-Token");
-			if (authToken) {
-				Mojo.Log.info("Authenticate succeeded: %s", transport.status);
-				this.httpClient.authToken = authToken;
-				if (persist) {
-					this.localStorage.setValue("authToken", authToken);
-					this.localStorage.setValue("username", username);
-				}
-				this.userIsAuthenticated = true;
-				if (onSuccess) {
-					onSuccess(transport);
-				}
-			} else {
-				Mojo.Log.error("HTTPClient reports success, but no auth token.");
-				onFailure(transport);
-			}
-		}.bind(this),
-
-		// Failure
-		onFailure);
-};
-
-AuthService.prototype.logout = function() {
-	this.localStorage.remove("authToken");
-	this.localStorage.remove("username");
-	this.userIsAuthenticated = false;
-	this.username = "";
-};
-
-AuthService.prototype.xmlEscape = function(string) {
-	string.replace(/&/g, "&amp;");
-	string.replace(/</g, "&lt;");
-	return string;
-};
-*/
